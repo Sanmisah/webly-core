b89ee0edf306b0ae4988ea8e6fa4694d356ce5f9
Sanjeev Divekar <sanjeev@sanmisha.com>
2022-11-26 14:33:20 +0530

    LC

--- src/Database/Seeds/InitWebsiteContent.php
+++ src/Database/Seeds/InitWebsiteContent.php
@@ -144,6 +144,21 @@ class InitWebsiteContent extends Seeder
 
         $this->db->table('pages')->insert($data);
 
+        //Add Page Blocks
+        $data = [
+            'page_id'   => 1,
+            'block'     => '<div class="row"> <div class="col-lg-12"> <h2 class="page-header">Modern Business Features</h2> </div> <div class="col-md-6"> <p>The Modern Business template by Start Bootstrap includes:</p> <ul> <li><strong>Bootstrap v3.3.7</strong></li> <li>jQuery v1.11.1</li> <li>Font Awesome v4.2.0</li> <li>Working PHP contact form with validation</li> <li>Unstyled page elements for easy customization</li> <li>17 HTML pages</li> </ul> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p> </div> <div class="col-md-6"><img class="img-responsive" src="https://dummyimage.com/700x450/#f1f1f/fff" alt="" /></div> </div>'
+        ];
+
+        $this->db->table('page_blocks')->insert($data);        
+
+        $data = [
+            'page_id'   => 1,
+            'block'     => '<div class="col-md-8"> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p> </div> <div class="col-md-4"><a class="btn btn-lg btn-primary btn-block" href="#">Call to Action</a></div>'
+        ];
+
+        $this->db->table('page_blocks')->insert($data);        
+
 
         // Add Main Menu Entries
         $data = [
