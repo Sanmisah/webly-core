005e74e7e27b09bd5f184bbdb082c42f8dd99b43
Sanjeev Divekar <sanjeev@sanmisha.com>
2024-01-20 17:59:42 +0530

    LC

--- src/Models/Orders.php
+++ src/Models/Orders.php
@@ -8,6 +8,6 @@ class Orders extends BaseModel
     protected $returnType       = \Webly\Core\Entities\Order::class;
     protected $allowedFields    = [
         'id', 'order_no', 'invoice_no', 'invoice_date', 'first_name', 'last_name', 'email', 'mobile_no',
-        'adddress_line_1', 'adddress_line_2', 'city', 'state', 'pincode', 'gross_amount', 'net_amount'
+        'address_line_1', 'address_line_2', 'city', 'state', 'pincode', 'gross_amount', 'net_amount'
     ];
 }
