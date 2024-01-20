97f49a9fbd1544e8cf63c77b83ad1189c4dffc41
Sanjeev Divekar <sanjeev@sanmisha.com>
2024-01-20 19:52:22 +0530

    Bug Fix

--- src/Models/Orders.php
+++ src/Models/Orders.php
@@ -7,7 +7,7 @@ class Orders extends BaseModel
     protected $table            = 'orders';
     protected $returnType       = \Webly\Core\Entities\Order::class;
     protected $allowedFields    = [
-        'id', 'order_no', 'invoice_no', 'invoice_date', 'first_name', 'last_name', 'email', 'mobile_no',
+        'id', 'order_no', 'invoice_no', 'invoice_date', 'first_name', 'last_name', 'email', 'mobile',
         'address_line_1', 'address_line_2', 'city', 'state', 'pincode', 'gross_amount', 'net_amount'
     ];
 }
